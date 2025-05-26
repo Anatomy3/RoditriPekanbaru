c2e1da98e1ead81016496e9f8fcd367a6f53d4b8
Vigio <daikudata@gmail.com>
2025-05-26 13:16:05 +0700

    up

--- obj/Release/net8.0/RoditriPekanbaru.AssemblyInfo.cs
+++ obj/Release/net8.0/RoditriPekanbaru.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoditriPekanbaru")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259dad9e58943dc5a8a895c73f36c999f1488e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoditriPekanbaru")]
